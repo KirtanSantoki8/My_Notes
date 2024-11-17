6f90e630b83ffbe22b85f91bd2e639d6c3e27cbc
KirtanPatel8 <kirtansantoki12@gmail.com>
2024-11-17 22:17:05 +0530

    Initial Commit

--- app/src/main/java/com/devkt/mynotes/MainActivity.kt
+++ app/src/main/java/com/devkt/mynotes/MainActivity.kt
@@ -1,12 +1,16 @@
 package com.devkt.mynotes
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +19,16 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        auth = FirebaseAuth.getInstance()
+
+        val logOutBtn = findViewById<Button>(R.id.button)
+        logOutBtn.setOnClickListener {
+            auth.signOut()
+            val intent = Intent(this,LoginActivity::class.java)
+            startActivity(intent)
+            finish()
         }
     }
 }
